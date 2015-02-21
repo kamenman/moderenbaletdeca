303bd8ad963813110ed83af8ee84d74222a88237
kamenman <kamenman@abv.bg>
2015-02-21 17:23:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitle='Детски спортен клуб ЗВЕЗДИ';
+include './header.php';
+?>
+<div class="container">
+    <div class="menu">
+        <a href="forus.php" ><h3>За нас</h3></a>
+        <a href="galerie.php" ><h3>Галерия</h3></a>
+         <a href="grafic.php" ><h3>График</h3></a>
+        <a href="contact.php" ><h3>Контакти</h3></a>
+    </div>
+       
+</div>
+<h1>Детско танцово<img src="logo1.jpg">студио "ЗВЕЗДИ"</h1>
+        <h2>Имаме удоволствието да Ви поканим да се присъедините </h2>
+        <h2 class="second">в часовете ни по Модерен балет за деца в кв. Лозенец. </h2>
+       
+        
+ <?php
+include './footer.php';
+?>
+
